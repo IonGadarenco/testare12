b7f517a7f3cf67031aac06b432cc05f810508996
DumitruGirdei <girdeidumitru@gmail.com>
2023-06-08 15:23:57 +0300

    COmitul lui DIma

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ const nume = document.querySelector("h1");
 nume.addEventListener("click", () => {
     nume.textContent = "ION";
 });
+
+
+const nume1 = document.querySelector("h2");
+
+nume1.addEventListener("click", () => {
+    nume1.textContent = "Dima";
+});
