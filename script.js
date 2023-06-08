c2fcc8c1593ca12aed542b776d9f3a6af8f18d09
Ion Gadarenco <ion.gadarenco@gmail.com>
2023-06-08 16:07:47 +0300

    adaugare de patrate la apasarea pe but

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ const nume1 = document.querySelector("h2");
 nume1.addEventListener("click", () => {
     nume1.textContent = "Dima";
 });
+
+const but = document.querySelector("button");
+const body = document.querySelector("body");
+const arrDima = ["D", "I", "M", "A"];
+let count = 0;
+
+but.addEventListener("click", () => {
+    const patrat = document.createElement("div");
+    patrat.classList.add("patrat");
+    body.append(patrat);
+    patrat.textContent = arrDima[count];
+    count++;
+});
